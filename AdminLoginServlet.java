809403e775e98bfc2563754e536c0b88373277bc
vikas potu <92099250+jvika@users.noreply.github.com>
2024-01-22 20:03:15 +0530

    Add files via upload

---
+++ AdminLoginServlet.java
@@ -0,0 +1,23 @@
+package test;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+@SuppressWarnings("serial")
+@WebServlet("/adminLog")
+public class AdminLoginServlet extends HttpServlet{
+@Override
+protected void doPost(HttpServletRequest req,HttpServletResponse res)throws
+ServletException,IOException{
+AdminBean ab = new AdminLoginDAO().login(req.getParameter("uname"),
+req.getParameter("pword"));
+if(ab==null) {
+req.setAttribute("msg","Invalid Login process...<br>");
+req.getRequestDispatcher("Home.jsp").forward(req, res);
+}else {
+HttpSession hs = req.getSession();//Creating new Session
+hs.setAttribute("abean", ab);
+req.getRequestDispatcher("AdminLogin.jsp").forward(req, res);
+}
+}
+}
---
+++ DBInfo.java
@@ -0,0 +1,9 @@
+package test;
+
+public interface DBInfo
+{
+public static final String dbUrl="jdbc:oracle:thin:@localhost:1521:orcl";
+public static final String uName="c##batch2pm";
+public static final String pWord="vikaspotu";
+
+}
